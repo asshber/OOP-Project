6d7dbf3dedddee63cedb581f19850b27cae728ea
Ali Ashber <aliashber52@gmail.com>
2021-04-06 21:35:22 +0500

    Add project files.

---
+++ Classes.cpp
@@ -0,0 +1,121 @@
+class two_wheeler
+{
+	double virtual getprice() {}
+};
+class suzuki :public two_wheeler
+{
+private:
+	friend class Admin;
+	double price;
+private:
+	double getprice() 
+	{
+		return price;
+	}
+};
+class yamaha :public two_wheeler
+{
+private:
+	friend class Admin;
+	double price;
+private:
+	double getprice()
+	{
+		return price;
+	}
+};
+class harley_davidson :public two_wheeler
+{
+private:
+	friend class Admin;
+	double price;
+private:
+	double getprice()
+	{
+		return price;
+	}
+};
+class four_wheeler
+{
+private:
+	double virtual getprice() {}
+};
+class audi :public four_wheeler
+{
+private:
+	friend class Admin;
+	double price;
+private:
+	double getprice()
+	{
+		return price;
+	}
+};
+class bmw : public four_wheeler
+{
+private:
+	friend class Admin;
+	double price;
+private:
+	double getprice()
+	{
+		return price;
+	}
+};
+class tesla : public four_wheeler
+{
+private:
+	friend class Admin;
+	double price;
+private:
+	double getprice()
+	{
+		return price;
+	}
+};
+class six_wheeler
+{
+private:
+	double virtual getprice() {}
+};
+class isuzu : public six_wheeler
+{
+private:
+	friend class Admin;
+		double price;
+private:
+	double getprice()
+	{
+		return price;
+	}
+};
+class hyundai : public six_wheeler
+{
+private:
+	friend class Admin;
+	double price;
+private:
+	double getprice()
+	{
+		return price;
+	}
+};
+class volvo : public six_wheeler
+{
+private:
+	friend class Admin;
+	double price;
+private:
+	double getprice()
+	{
+		return price;
+	}
+};
+class Admin
+{
+
+};
+int main()
+{
+
+}
---
+++ allheaders.h
@@ -0,0 +1,6 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<istream>
+#include<ostream>
+using namespace std;
